b362e1f4060ddf89a36deff149a1762965c9c892
Saketh Dargula <sdargula@gmail.com>
2021-03-26 11:42:26 -0400

    initalized sheet api stuff

--- app/src/main/java/com/carebase/scannerexploration/ResultFragment.java
+++ app/src/main/java/com/carebase/scannerexploration/ResultFragment.java
@@ -8,7 +8,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,14 +18,30 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -47,9 +62,9 @@ public class ResultFragment extends Fragment {
 
         MaterialToolbar toolbar = rootView.findViewById(R.id.toolbar);
         toolbar.setNavigationOnClickListener((view) -> requireActivity().getSupportFragmentManager().popBackStack());
+
         PictureAnalysisActivity mainActivity = (PictureAnalysisActivity) requireActivity();
         String fileName = requireArguments().getString("file_name");
-
 
         // get file
         File photoFile = mainActivity.getPhotoFileUri(fileName);
@@ -76,6 +91,50 @@ public class ResultFragment extends Fragment {
                     Toast.makeText(getContext(),"Image processing failed", Toast.LENGTH_LONG).show();
                 });
         return rootView;
+    }
+
+    private static final String APPLICATION_NAME = "Device Name Annotator";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+    private static final String CREDENTIALS_FILE_PATH = "../../../credentials.json";
+    private static final String SPREADSHEET_ID = "";
+
+    private Credential getCredentials(final NetHttpTransport transport) {
+        // Load client secrets.
+        InputStream in = ResultFragment.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            Log.d(ResultFragment.class.getSimpleName(),"Resource not found: " + CREDENTIALS_FILE_PATH);
+            return null;
+        }
+        try {
+            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                    transport, JSON_FACTORY, clientSecrets, SCOPES)
+                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                    .setAccessType("offline")
+                    .build();
+            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+            return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void onSaveAnnotations() {
+        try {
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            Credential credential = getCredentials(HTTP_TRANSPORT);
+            if (credential == null) {
+                return;
+            }
+            Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
